98db6d380b11b5295a288f748ca3527e55cb7783
carolinajg66 <carolinajustinianoguzman@gmail.com>
2017-11-17 14:00:13 +0100

    EXAMEN PMM

---
+++ EXAMENCAROLINA/app/src/main/java/com/example/anaguz/examencarolina/Pizza.java
@@ -0,0 +1,46 @@
+package com.example.anaguz.examencarolina;
+
+import java.io.Serializable;
+
+/**
+ * Created by anaguz on 17/11/17.
+ */
+
+
+public class Pizza implements Serializable {
+
+    private  String nombre;
+    private  String descripcion;
+    private  double precio;
+
+    public Pizza (String nombre, String descripcion, double precio){
+
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
---
+++ EXAMENCAROLINA/app/src/main/java/com/example/anaguz/examencarolina/acercade.java
@@ -0,0 +1,30 @@
+package com.example.anaguz.examencarolina;
+
+/**
+ * Created by anaguz on 17/11/17.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class acercade extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acercade);
+
+        Button volver = (Button)findViewById(R.id.volver);
+
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent volver = new Intent(acercade.this, MainActivity.class);
+                startActivity(volver);
+            }
+        });
+    }
+}
