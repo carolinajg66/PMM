1dac4a668c41b9e6dbbbe1ba6d5f604ae9f4cddf
carolinajg66 <carolinajustinianoguzman@gmail.com>
2017-12-05 12:11:50 +0100

    Actualizado SoloBiciDidactico

---
+++ SoloBiciDidactico/app/src/main/java/com/example/anaguz/solobicididactico/AcercaDe.java
@@ -0,0 +1,18 @@
+package com.example.anaguz.solobicididactico;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by anaguz on 5/12/17.
+ */
+
+public class AcercaDe extends Activity {
+
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+//Hacemos visible la interfaz/layoutque se encuentra en acercade.xml
+        setContentView(R.layout.acercade);
+    }
+}
---
+++ SoloBiciDidactico/app/src/main/java/com/example/anaguz/solobicididactico/Juego.java
@@ -0,0 +1,17 @@
+package com.example.anaguz.solobicididactico;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by anaguz on 5/12/17.
+ */
+
+public class Juego extends Activity{
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.juego);
+
+    }
+}
